6b6d89ba1e41e3043f3d6f55a4a6be69a89c352b
Austin <austin007n@gmail.com>
2026-01-26 17:30:23 -0600

    Completed Basic Item Updating Functions

--- app.py
+++ app.py
@@ -39,24 +39,26 @@ def update_book(title, column, value):
         c.execute(f"UPDATE books set {column} = (?) WHERE title = (?)", (value, title))
 
 def prompt_update():
-    title = input("Enter name of book to update: ").strip().lower()
+    title = input("Enter title of book to update: ").strip().lower()
     while not title:        
         print("Title cannot be empty. Enter a title.")
-        title = input("\nEnter name of book to update: ").strip().lower()
+        title = input("\nEnter title of book to update: ").strip().lower()
     print("\n")
 
-    # need to fix:
     column = input("Change Data:\nTitle [1]\nAuthor[2]\nEnter Number: ")
-    while column not in [1 ,2]:
+    while column not in ["1" ,"2"]:
         print("Invalid Choice. Choose from (1-2)")
         column = input("\nChange Data:\nTitle [1]\nAuthor[2]\nEnter Number: ")
     print("\n")
-    #
+    if column == "1": 
+        column = "title"
+    else: 
+        column = "author"
 
-    value = input("Enter new value: ")
+    value = input("Enter new value: ").strip().lower()
     while not value: 
         print("Value cannot be empty. Enter a value.")
-        value = input("\nEnter new value: ")
+        value = input("\nEnter new value: ").strip().lower()
     print("\n")
 
     return title, column, value
@@ -67,9 +69,13 @@ def get_book(title):
         c.execute("SELECT * FROM books WHERE title=(?)", (title,))
         print(c.fetchone())
 
-add_book("Test 2", "Sir Bubbles")
-add_book("Test 1", "Sir Bubbles")
-get_book("Test 1")
+add_book("Test 1".lower(), "John Doe".lower())
+
+title, column, value = prompt_update()
+update_book(title, column, value)
+
+add_book("Test 2", "Jane Doe")
+get_book("Test 3".lower())
 
 
 conn.commit()
