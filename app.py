e12fb0276d494ff0c588e358bb04e50eaf48e5d1
Austin <austin007n@gmail.com>
2026-01-26 15:45:40 -0600

    Added Update and Delete Functions

--- app.py
+++ app.py
@@ -18,21 +18,48 @@ def error_handling(func):
         try:
             return func(*args, **kwargs)
         except Exception as e:
-            print("Error", e)
+            print("Error: ", e)
     return wrapper
 
 @error_handling
 def add_book(title, author):
     with conn:
         c.execute("INSERT INTO books (title, author) VALUES (?, ?)", (title, author))
+        print(f"{title} added.\n")
 
 @error_handling
-def delete_book():
-    pass
+def delete_book(title):
+    with conn:
+        c.execute("DELETE FROM books WHERE title=(?)", (title,))
+        print(f"{title} deleted.\n")
 
 @error_handling
-def update_book():
-    pass
+def update_book(title, column, value):
+    with conn:
+        c.execute(f"UPDATE books set {column} = (?) WHERE title = (?)", (value, title))
+
+def prompt_update():
+    title = input("Enter name of book to update: ").strip().lower()
+    while not title:        
+        print("Title cannot be empty. Enter a title.")
+        title = input("\nEnter name of book to update: ").strip().lower()
+    print("\n")
+
+    # need to fix:
+    column = input("Change Data:\nTitle [1]\nAuthor[2]\nEnter Number: ")
+    while column not in [1 ,2]:
+        print("Invalid Choice. Choose from (1-2)")
+        column = input("\nChange Data:\nTitle [1]\nAuthor[2]\nEnter Number: ")
+    print("\n")
+    #
+
+    value = input("Enter new value: ")
+    while not value: 
+        print("Value cannot be empty. Enter a value.")
+        value = input("\nEnter new value: ")
+    print("\n")
+
+    return title, column, value
 
 @error_handling
 def get_book(title):
