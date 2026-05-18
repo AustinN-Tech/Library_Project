1a4de1515d3bf2ea1a47baa5494363e7439dfc20
AustinN-Tech <austin007n@gmail.com>
2026-05-17 19:10:06 -0500

    Added Functionality to Buttons & CSS Styling to PDF Viewer

--- src/static/read_book.js
+++ src/static/read_book.js
@@ -1,30 +1,60 @@
 import * as pdfjsLib from "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.mjs";
 
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.worker.mjs';
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.worker.mjs'; // handles heavy PDF work in the background, necessary for smooth viewing
 
-let loading_task = pdfjsLib.getDocument({ url: pdf_URL});
-loading_task.promise.then((pdf) => {
+// Defining Page/PDF Variables:
+let pdf_doc = null;
+let page_number = 1;
+const min_page = 1;
+let max_page = 1;
+let scale = 1.5; // controls zoom of PDF
+
+let canvas = document.querySelector("canvas"); // html drawing surface, pdf.js draws pdfs onto canvas
+const context = canvas.getContext('2d'); // "drawing tool" for the canvas
+
+let page_text = document.querySelector(".page_number");
+const previous_button = document.querySelector(".previous");
+const next_button = document.querySelector(".next");
+
+
+pdfjsLib.getDocument({ url: pdf_URL}).promise.then((pdf) => { // loading PDF doc takes time, must be a promise
     console.log("PDF loaded.");
 
-    let page_number = 1;
-    pdf.getPage(page_number).then((page) => {
-        console.log("Page loaded.");
+    pdf_doc = pdf; // load pdf into variable to reuse
+    max_page = pdf.numPages;
+    load_page(page_number);
+});
 
-        let scale = 1.5;
-        let viewport = page.getViewport( {scale: scale});
+function load_page(page_number) {
+    pdf_doc.getPage(page_number).then((page) => { // loading page takes time, must be a promise
+        console.log("PDF loaded.");
 
-        let canvas = document.querySelector("canvas");
-        let context = canvas.getContext('2d');
+        let viewport = page.getViewport( {scale: scale}); // viewport defines how PDF should appear
+
+        // set canvas dimensions to page viewport: canvas matches page size
         canvas.height = viewport.height;
-        canvas.width = viewport.width
+        canvas.width = viewport.width;
 
-        let renderContext = {
+        let renderContext = { // tells PDF.js to draw on the canvas using appropriate context, matching viewport
             canvasContext: context,
             viewport: viewport
         };
-        let renderTask = page.render(renderContext);
-        renderTask.promise.then(() => {
-            console.log('Page rendered.')
+        let renderTask = page.render(renderContext); // draws PDF page on canvas
+        renderTask.promise.then(() => { // render takes time, must be a promise
+            console.log('Page rendered.');
+            page_text.innerHTML = `Page ${page_number} of ${max_page}`; // updates page counter
         });
-    });
+    }); 
+}
+
+previous_button.addEventListener("click", () => {
+    if (page_number > min_page) {
+        load_page(--page_number);
+    }
+});
+
+next_button.addEventListener("click", () => {
+    if (page_number < max_page) {
+        load_page(++page_number);
+    }
 });
